fcb08d19336ee56ed2b6991628ba17444144da05
Saqib Ramzan <sawkey.qhan@gmail.com>
2013-09-05 20:35:16 +0500

    change seed (heroku yaml issue)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,13 @@
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
 puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+#YAML.load(ENV['ROLES']).each do |role|
+%w(admin, user, VIP).each do |role|
+  Role.find_or_create_by_name({:name => role}, :without_protection => true)
   puts 'role: ' << role
 end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.confirm!
-user.add_role :admin
+#puts 'DEFAULT USERS'
+#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+#puts 'user: ' << user.name
+#user.confirm!
+#user.add_role :admin
